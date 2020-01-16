552078c03cad2b87b940c0d47a878c0543241ac2
Andrew Wilk <andrewwilk@modv-ve703-15427.apn.wlan.private.upenn.edu>
2020-01-16 14:05:00 -0500

    brought in code for batch subject/sample"

--- delete_this_file.py
+++ delete_this_file.py
@@ -121,3 +121,60 @@ def build_sample_file():
 ## building a 5 phenotype obj list of objects and manually dropping them into gen3 works 1/16 !!!if!!! there are no duplicate
 ##phenotype objects, but for some reason several times in tests there'll be a subjects phenotype data that gets duplicated,
 ## causes load failure for the list
+
+
+##
+"""subject and sample code below"""
+
+def run_subject_writer(consent="HMB-IRB-PUB"):
+    subject_array = []
+
+    def create_subject(subject, current_subject_id):
+        fetched_project_id = "218603c7-fe41-56e9-9ccd-f08daf002162"
+        subject_obj = {
+            "cohort": subject["cohort_key"], 
+            "projects": {
+                "id": fetched_project_id
+            }, 
+            "consent": subject["consent"]["key"], 
+            "type": "subject", 
+             "submitter_id": subject["key"]
+        }
+        subject_array.append(subject_obj)
+        
+    project_sample_set = set({})
+    with open("jsondumps/samplesSubjects.json", "r") as json_file:    
+        samplesAndSubjects = json.load(json_file)
+
+    for key, sample in samplesAndSubjects.iteritems():
+        ## some subjects have 'null' consent, ignoring for now
+        if sample["subject"]["consent"] is not None:
+            ## if the subject's consent matches the current project, add to the pss set
+            if sample["subject"]["consent"]["key"].strip() == consent:
+                project_sample_set.add( sample["subject"]["key"] )
+    
+    for dictkey, subject_id in enumerate(project_sample_set):
+        ## AW - why doesn't this create two subjects for subjects with multiple samples...but if tied to same subject entity (with submitter id) in db then maybe overwrites with same info?
+
+        for samplekey, sample in samplesAndSubjects.iteritems():
+            if sample["subject"]["key"] == subject_id:
+                subject = sample["subject"]
+                current_subject_id = subject["key"]
+
+                create_subject(subject, current_subject_id)
+
+            if len(subject_array) > 0 and len(subject_array) % 5 == 0:
+                print('-----')
+                print(subject_array)
+                # submitter.submit_record(program_name, project_name, sample_array)
+                print('sending 5 samples')
+                subject_array = []
+                print(subject_array)
+
+
+def build_sample_file():
+    subjectsAndSamples = getSamplesSubjects(1)
+    print(len(subjectsAndSamples))
+
+
+## building a 5 subject list of objects and manually dropping them into gen3 works 1/16
